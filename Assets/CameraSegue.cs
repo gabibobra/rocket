856d1f1c106a3cb1d626efe5dfb8816f815f4e69
Gabriela Bobra Carvalho <gabibobra@gmail.com>
2019-11-26 20:36:41 -0300

    first commit

---
+++ Assets/CameraSegue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSegue : MonoBehaviour
+{
+    public Transform target;
+    public float control;
+    public Vector3 Distance;
+    private Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        control = target.position.y;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (control <= target.position.y)
+        {
+            transform.position = new Vector3(target.position.x, target.position.y, target.position.z) + Distance;
+        }
+    }
+}
---
+++ Assets/ParachuteController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParachuteController : MonoBehaviour
+
+{
+    public Rigidbody paraquedas;
+    public Vector3 rot;
+    public GameObject body;
+    public bool check2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paraquedas = GetComponent<Rigidbody>();
+        paraquedas.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        check2 = body.GetComponent<ControlaFoguete>().check;
+        if (check2 == true)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                paraquedas.GetComponent<MeshRenderer>().enabled = true;
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(90f,0,0), 5f * Time.deltaTime);
+                paraquedas.drag = 10f;
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0,90f,0), 1f * Time.deltaTime);
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, -90f, 0), 1f * Time.deltaTime);
+            }
+        }
+    }
+}
